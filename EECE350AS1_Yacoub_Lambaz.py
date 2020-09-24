db1ba49401da2aa84fd467011b4cfc974fe22973
DooofusRick <65854079+DooofusRick@users.noreply.github.com>
2020-09-24 20:52:27 +0300

    Update and rename EECE350AS1.py to EECE350AS1_Yacoub_Lambaz.py

--- EECE350AS1_Yacoub_Lambaz.py
+++ EECE350AS1_Yacoub_Lambaz.py
@@ -2,16 +2,10 @@ import socket
 import struct,time
 import sys
 
-def dprint(s):
-    for c in s:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.05)
-
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(("129.6.15.27",37))
 
-dprint("Listening to IP address: 129.6.15.27\n\n")
+print("Listening to IP address: 129.6.15.27\n\n")
 time_since_1970 = 2208988800
 
 t = mysock.recv(4)
@@ -22,12 +16,12 @@ final_time_1_l = final_time_1.split()
 final_time_1 = final_time_1_l[3]
 mysock.close()
 
-dprint("NIST Server Time in IP address 129.6.15.27 is " + str(final_time_1) +"\n\n")
-time.sleep(1)
+print("NIST Server Time in IP address 129.6.15.27 is " + str(final_time_1) +"\n\n")
+
 mysock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock2.connect(("129.6.15.30",37))
 
-dprint("Listening to IP address: 132.163.97.1\n\n")
+print("Listening to IP address: 132.163.97.1\n\n")
 
 t2 = mysock2.recv(4)
 t2 = struct.unpack("!I",t2)[0]
@@ -38,7 +32,7 @@ final_time_2 = final_time_2_l[3]
 mysock.close()
 
 
-dprint("NIST Server Time in IP address 132.163.97.1 is " + str(final_time_2)+'\n\n')
+print("NIST Server Time in IP address 132.163.97.1 is " + str(final_time_2)+'\n\n')
 
 
-dprint("The two IP addresses are "+ str(abs(t2-t))+ " seconds apart\n\n")
+print("The two IP addresses are "+ str(abs(t2-t))+ " seconds apart\n\n")
